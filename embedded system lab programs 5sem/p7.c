ebf80283b8722be4d1c86214277407c8cf8c54b3
AbhiKarma1 <124451257+abhishikkarmakar@users.noreply.github.com>
2023-11-15 23:51:17 +0530

    Update p7.c

--- embedded system lab programs 5sem/p7.c
+++ embedded system lab programs 5sem/p7.c
@@ -1,45 +1,44 @@
 #include <stdio.h>
 #include <pthread.h>
-
 #define Buffer_Limit 10
-
-int Index_Value = 0, i;
+int Index_Value = 0,i, j;
 int Buffer[Buffer_Limit];
-
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t Buffer_Full = PTHREAD_COND_INITIALIZER;
 pthread_cond_t Buffer_Empty = PTHREAD_COND_INITIALIZER;
-
-void *Consumer() {
-    for (i = 1; i <= Buffer_Limit; i++) {
-        pthread_mutex_lock(&mutex);
-        if (Index_Value == 0) {
-            pthread_cond_wait(&Buffer_Empty, &mutex);
-        }
-        printf("\nConsumer:%d\t", Index_Value--);
-        pthread_mutex_unlock(&mutex);
-        pthread_cond_signal(&Buffer_Full);
-    }
+void *Consumer()
+{
+for(j=0;j<Buffer_Limit;j++)
+{pthread_mutex_lock(&mutex);
+if(Index_Value == -1)
+{
+pthread_cond_wait(&Buffer_Empty, &mutex);
 }
-
-void *Producer() {
-    for (i = 1; i <= Buffer_Limit; i++) {
-        pthread_mutex_lock(&mutex);
-        if (Index_Value == Buffer_Limit) {
-            pthread_cond_wait(&Buffer_Full, &mutex);
-        }
-        Buffer[Index_Value++] = i;
-        printf("\nProducer:%d\t", Index_Value);
-        pthread_mutex_unlock(&mutex);
-        pthread_cond_signal(&Buffer_Empty);
-    }
+printf("\nConsumer:%d\t", Index_Value--);
+pthread_mutex_unlock(&mutex);
+pthread_cond_signal(&Buffer_Full);
 }
-
-int main() {
-    pthread_t producer_thread_id, consumer_thread_id;
-    pthread_create(&producer_thread_id, NULL, &Producer, NULL);
-    pthread_create(&consumer_thread_id, NULL, &Consumer, NULL);
-    pthread_join(producer_thread_id, NULL);
-    pthread_join(consumer_thread_id, NULL);
-    return 0;
+}
+void *Producer()
+{
+for(i=0;i<Buffer_Limit;i++)
+{
+pthread_mutex_lock(&mutex);
+if(Index_Value == Buffer_Limit)
+{
+pthread_cond_wait(&Buffer_Full, &mutex);
+}
+Buffer[Index_Value++] = rand()%50;
+printf("\nProducer:%d\t", Index_Value);
+pthread_mutex_unlock(&mutex);
+pthread_cond_signal(&Buffer_Empty);
+}
+}
+int main()
+{ pthread_t producer_thread_id, consumer_thread_id;
+pthread_create(&producer_thread_id, NULL, &Producer, NULL);
+pthread_create(&consumer_thread_id, NULL, &Consumer, NULL);
+pthread_join(producer_thread_id, NULL);
+pthread_join(consumer_thread_id, NULL);
+return 0;
 }
